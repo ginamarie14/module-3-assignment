9d373fe732a10873a964934daefb4d0e08b2db6f
Gina Marie <ginamarie214@gmail.com>
2022-07-06 01:26:28 -0500

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Assignment code here
-var length = 12;
-var characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890~!@#$%^&*(),./;:<>?`";
-
 
 // Get references to the #generate element
 function generatePassword(){
-    
+    var length = 12;
+    var characters = [Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,1,2,3,4,5,6,7,8,9,0,'~','!','@','#','$','%','^','&','*','(',')',',','.','/',';',':','<','>','?','`'];
+    Math.random()*n;
     var generateBtn = document.querySelector("#generate");
 }
 
