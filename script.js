0176a42ac3829a5537758b7d871d9f9ceaf4e235
Gina Marie <ginamarie214@gmail.com>
2022-07-06 01:12:02 -0500

    index.html

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment code here
+var length = 12;
+var characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890~!@#$%^&*(),./;:<>?`";
+
+
+// Get references to the #generate element
+function generatePassword(){
+    
+    var generateBtn = document.querySelector("#generate");
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
